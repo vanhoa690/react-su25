dd25c92c5a5fb15ba72e91e08d7971a139e23cbb
vanhoa690 <vanhoa690@gmail.com>
2025-08-06 15:10:19 +0700

    check user id 2

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -3,8 +3,11 @@ import { Outlet } from "react-router-dom";
 import AccessDenied from "./AccessDenied";
 
 const PrivateRoute: React.FC = () => {
-  const token = localStorage.getItem("token");
-  return token ? <Outlet /> : <AccessDenied />;
+  //   const token = localStorage.getItem("token");
+  // user.id === 2
+  const user = JSON.parse(localStorage.getItem("user") || "{}");
+
+  return user?.id === 2 ? <Outlet /> : <AccessDenied />;
 };
 
 export default PrivateRoute;
